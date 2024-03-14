c4f70e48365d0644bcd24b19f6835f375b52aa25
ZealanL <zlaferte@gmail.com>
2024-03-13 18:14:05 -0700

    RLGymSim: Make DefaultOBS support vector pos coefficient

--- RLGymPPO_CPP/RLGymSim_CPP/src/RLGymSim_CPP/Utils/OBSBuilders/DefaultOBS.h
+++ RLGymPPO_CPP/RLGymSim_CPP/src/RLGymSim_CPP/Utils/OBSBuilders/DefaultOBS.h
@@ -6,9 +6,12 @@ namespace RLGSC {
 	class DefaultOBS : public OBSBuilder {
 	public:
 
-		float posCoef, velCoef, angVelCoef;
+		Vec posCoef;
+		float velCoef, angVelCoef;
 		DefaultOBS(
-			float posCoef = 1 / CommonValues::BACK_WALL_Y, float velCoef = 1 / CommonValues::CAR_MAX_SPEED, float angVelCoef = 1 / CommonValues::CAR_MAX_ANG_VEL
+			Vec posCoef = Vec(1 / CommonValues::SIDE_WALL_X, 1 / CommonValues::BACK_WALL_Y, 1 / CommonValues::CEILING_Z),
+			float velCoef = 1 / CommonValues::CAR_MAX_SPEED,
+			float angVelCoef = 1 / CommonValues::CAR_MAX_ANG_VEL
 		) : posCoef(posCoef), velCoef(velCoef), angVelCoef(angVelCoef) {
 
 		}
