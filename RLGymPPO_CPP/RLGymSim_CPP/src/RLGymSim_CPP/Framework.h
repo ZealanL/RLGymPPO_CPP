481978c929cb262990c9935491ce2f89f59c73f7
ZealanL <zlaferte@gmail.com>
2024-03-15 20:37:37 -0700

    RLGymSim: Add RG_ASSERT and RG_PARA_ASSERT macros

--- RLGymPPO_CPP/RLGymSim_CPP/src/RLGymSim_CPP/Framework.h
+++ RLGymPPO_CPP/RLGymSim_CPP/src/RLGymSim_CPP/Framework.h
@@ -11,4 +11,21 @@
 className(const className&) = delete;  \
 className& operator= (const className&) = delete
 
-#define RG_ERR_CLOSE(s) { std::string _errorStr = RS_STR("RG FATAL ERROR: " << s); RG_LOG(_errorStr); throw std::runtime_error(_errorStr); exit(EXIT_FAILURE); }
+#define RG_ERR_CLOSE(s) { \
+std::string _errorStr = RS_STR("RG FATAL ERROR: " << s); \
+RG_LOG(_errorStr); \
+throw std::runtime_error(_errorStr); \
+exit(EXIT_FAILURE); \
+}
+
+#ifndef RG_UNSAFE
+#define RG_ASSERT(cond) { if (!(cond)) { RG_ERR_CLOSE("Assertion failed: " << #cond); } }
+#else
+#define RG_ASSERT(cond) {}
+#endif
+
+#ifdef RG_PARANOID_MODE
+#define RG_PARA_ASSERT(cond) RG_ASSERT(cond)
+#else
+#define RG_PARA_ASSERT(cond) {}
+#endif
